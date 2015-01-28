646aa9112a340b1fb86a8c2b76ec898370880e9f
nertro <schneenerdchen@googlemail.com>
2015-01-28 18:40:37 +0100

    New: Pitchfork, Food, Taskliste für Farmer

--- PigglyWiggly/Assets/Scripts/FarmerMovement.cs
+++ PigglyWiggly/Assets/Scripts/FarmerMovement.cs
@@ -6,15 +6,15 @@ public class FarmerMovement : MonoBehaviour {
 
     public float speed = 5f;
 
-    List<GameObject> pigs;
     Vector3 targetPosition;
     List<Activities> taskList;
+    GameObjectAdmin objectAdmin;
 
-    enum Activities { Cleaning, Feeding, Idle, MoveTo };
+    enum Activities { Cleaning, Feeding, Idle, MoveToPitchfork, MoveToPig, MoveToFood };
     Activities currentActivity;
 
 	void Start () {
-        pigs = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameObjectAdmin>().pigs;
+        objectAdmin = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameObjectAdmin>();
         targetPosition = this.transform.position;
         currentActivity = Activities.Idle;
         taskList = new List<Activities>();
@@ -26,12 +26,23 @@ public class FarmerMovement : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.E))
         {
             taskList.Clear();
-            taskList.Add(Activities.MoveTo(
+            taskList.Add(Activities.MoveToPitchfork);
+            taskList.Add(Activities.MoveToPig);
+            taskList.Add(Activities.Cleaning);
+
+            currentActivity = taskList[0];
+            DoTask();
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            currentActivity = Activities.Feeding;
+            taskList.Clear();
+            taskList.Add(Activities.MoveToPitchfork);
+            taskList.Add(Activities.MoveToPig);
+            taskList.Add(Activities.Cleaning);
+
+            currentActivity = taskList[0];
+            DoTask();
         }
     }
 
@@ -40,23 +51,23 @@ public class FarmerMovement : MonoBehaviour {
         Debug.Log(currentActivity);
         if (currentActivity != Activities.Idle && this.GetComponent<NavMeshAgent>().remainingDistance <= 0.5)
         {
+            taskList.Remove(taskList[0]);
+            currentActivity = Activities.Idle;
+
             this.GetComponent<NavMeshAgent>().Stop();
             this.GetComponent<NavMeshAgent>().updatePosition = false;
             this.GetComponent<NavMeshAgent>().updateRotation = false;
             Debug.Log("Bumm");
 
-            DoTask();
+            if (taskList.Count <= 0)
+            {
+                currentActivity = Activities.Idle;
+            }
+            else
+            {
+                DoTask();
+            }
         }
-    }
-
-    void MoveToP(Vector3 position)
-    {
-        targetPosition = position;
-        //targetPosition = pigs[this.GetComponent<SelectPig>().currentPig].transform.position;
-        //rigidbody.velocity = (this.targetPosition - this.transform.position).normalized * speed;
-        this.GetComponent<NavMeshAgent>().updatePosition = true;
-        this.GetComponent<NavMeshAgent>().updateRotation = true;
-        this.GetComponent<NavMeshAgent>().SetDestination(targetPosition);
     }
 
     void DoTask()
@@ -69,19 +80,35 @@ public class FarmerMovement : MonoBehaviour {
         {
             Clean();
         }
+        else if(currentActivity == Activities.MoveToPig)
+        {
+            MoveTo(objectAdmin.pigs[this.GetComponent<SelectPig>().currentPig].transform.position);
+        }
+        else if (currentActivity == Activities.MoveToPitchfork)
+        {
+            MoveTo(objectAdmin.pitchfork.transform.position);
+        }
+        else if (currentActivity == Activities.MoveToFood)
+        {
+            MoveTo(objectAdmin.food.transform.position);
+        }
     }
 
     void Feed()
     {
         Debug.Log("Feed");
-
-        currentActivity = Activities.Idle;
     }
 
     void Clean()
     {
         Debug.Log("Clean");
+    }
 
-        currentActivity = Activities.Idle;
+    void MoveTo(Vector3 position)
+    {
+        targetPosition = position;
+        this.GetComponent<NavMeshAgent>().updatePosition = true;
+        this.GetComponent<NavMeshAgent>().updateRotation = true;
+        this.GetComponent<NavMeshAgent>().SetDestination(targetPosition);
     }
 }
