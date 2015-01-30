c9bfbb2781b7641770f180ce9e2bedfd2bc5221f
nertro <schneenerdchen@googlemail.com>
2015-01-30 09:10:28 +0100

    New: Menu, Controls expl. Changed: Background im Level ist UI

---
+++ PigglyWiggly/Assets/Scripts/ShowHideMenuOverlay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHideMenuOverlay : MonoBehaviour {
+
+    public GameObject panelToShow;
+    public GameObject panelToHide;
+
+    void OnClick()
+    {
+        panelToShow.SetActive(true);
+        panelToHide.SetActive(false);
+    }
+}
